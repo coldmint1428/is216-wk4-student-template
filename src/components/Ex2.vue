9de426658aafd0184ca2da4f316241b496fa8290
coldmint1428 <chwkm7656@gmail.com>
2026-09-06 17:13:31 +0800

    completed ex1 and ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,11 +2,12 @@
     import { ref } from 'vue'
 
     // Add code here
+    const me = ref('/public/assets/me.png')
 
 </script>
 
 <template>
 
     <!-- Add code here -->
-   
+   <img v-bind:src="me">
 </template>
