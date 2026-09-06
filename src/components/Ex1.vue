3291f8de8d5d4f5be6ca661b8717ce2541a5ab13
coldmint1428 <chwkm7656@gmail.com>
2026-09-06 17:17:58 +0800

    update ex 1 and ex 2

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -4,7 +4,7 @@
     // Add code here
     // challenge: use array  
 
-    const message = ref('')
+    const message = ref('Choose your favourite fruit: ')
     const fruit1 = ref('apple')
     const fruit2 = ref('orange')
     const fruit3 = ref('peach')
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,12 +2,18 @@
     import { ref } from 'vue'
 
     // Add code here
-    const me = ref('/public/assets/me.png')
+    const me = ref('/assets/me.png')
+    const width = ref(300)
+    const height = ref(200)
 
 </script>
 
 <template>
 
     <!-- Add code here -->
-   <img v-bind:src="me">
+    <!-- single attribute binding: each attribute bound separately -->
+    <img v-bind:src="me" v-bind:width="width" v-bind:height="height">
+
+    <!-- multiple attribute binding: one object holds all the attributes at once -->
+    <!-- <img v-bind="{ src: me, width: width, height: height }"> -->
 </template>
