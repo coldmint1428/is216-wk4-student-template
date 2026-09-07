6ebcbcb2d4dfc6faa05f1a61f75c19abf0ae7c86
coldmint1428 <chwkm7656@gmail.com>
2026-09-07 09:43:05 +0800

    Update Ex3.vue

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -11,11 +11,11 @@
     <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
     <select v-model="selected">
-        <option selected>Username login</option>
-        <option>Email login</option>
+        <option selected value="username">Username login</option>
+        <option value="email">Email login</option>
     </select>
     
-    <div v-if="selected === 'Username login'">
+    <div v-if="selected === 'username'">
         <label>Username
         <input placeholder="Enter your username">
     </label>
